b46e7dce0bd912b447557f8706ac2ffa276857e8
DenzelHawking <denzelhawking@gmail.com>
2019-10-21 15:25:57 +0500

    toggle hide menu

---
+++ js/menu.js
@@ -0,0 +1,21 @@
+let menuButton = document.querySelector('#menu-button');
+let backgroundMenu = document.querySelector('#background-menu');
+
+menuButton.addEventListener('click', openMenu);
+backgroundMenu.addEventListener('click', closeMenu);
+
+function openMenu() {
+  document.body.classList.add('open-menu');
+  backgroundMenu.style.display = 'block';
+  setTimeout(function () {
+    document.body.classList.add('background-opacity');
+  }, 0);
+};
+
+function closeMenu() {
+  document.body.classList.remove('background-opacity');
+  document.body.classList.remove('open-menu');
+  setTimeout(function () {
+    backgroundMenu.style.display = 'none';
+  }, 500);
+};
