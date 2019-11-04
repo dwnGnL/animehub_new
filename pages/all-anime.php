15c0585ba460c0e671a0e8c24eabf72b7135ea0a
DenzelHawking <denzelhawking@gmail.com>
2019-11-04 15:35:34 +0500

    switch page added

--- pages/all-anime.php
+++ pages/all-anime.php
@@ -412,9 +412,23 @@
           <div class="film-gener">Жанр фильма</div>
         </div>
       </div>
-
-
     </div>
   </div>
+
+  <ul class="switch-page">
+    <li class="switch-page-item switch-page-active">1</li>
+    <li class="switch-page-item">2</li>
+    <li class="switch-page-item">3</li>
+    <li class="switch-page-item">4</li>
+    <li class="switch-page-item">5</li>
+    <li class="switch-page-item">6</li>
+    <li class="switch-page-item">7</li>
+    <li class="switch-page-item">8</li>
+    <li class="switch-page-item">9</li>
+    <li class="switch-page-item">10</li>
+    <li>...</li>
+    <li class="switch-page-item">25</li>
+  </ul>
+
 
 </div>
