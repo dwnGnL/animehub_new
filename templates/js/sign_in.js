84bc58d6462fc4a5b27392a0ae3ac44da9a781e4
DenzelHawking <denzelhawking@gmail.com>
2019-12-12 17:28:45 +0500

    focus modified

--- templates/js/sign_in.js
+++ templates/js/sign_in.js
@@ -16,44 +16,34 @@ if (signIn){
 
 function openProfile() {
   document.body.classList.add('open-profile');
-  setTimeout(function () {
-    document.body.classList.add('sign-in-opacity');
-  }, 0);
+  setTimeout(() => document.body.classList.add('sign-in-opacity'), 0);
 };
 
 function closeProfile() {
   document.body.classList.remove('sign-in-opacity');
-  setTimeout(function () {
-    document.body.classList.remove('open-profile');
-  }, 500);
+  setTimeout(() => document.body.classList.remove('open-profile'), 500);
 };
 
 function openSignIn() {
   document.body.classList.add('sign-in');
-  setTimeout(function () {
-    document.body.classList.add('sign-in-opacity');
-  }, 0);
+  setTimeout(() => document.body.classList.add('sign-in-opacity'), 0);
 };
 
 function closeSignIn() {
   document.body.classList.remove('sign-in-opacity');
-  setTimeout(function () {
-    document.body.classList.remove('sign-in');
-  }, 500);
+  setTimeout(() => document.body.classList.remove('sign-in-opacity'), 500);
 };
 
 
 let signInInput = document.querySelectorAll('.sign-in-input');
 let mainInput = document.querySelectorAll('.sign-in-input-item');
+let registInput = document.querySelectorAll('.registration-input');
+let mainRegistInput = document.querySelectorAll('.registration-input-item');
 
-mainInput.forEach((elem, index) => {
-  elem.onfocus = () => {
-    signInInput[index].classList.add('sign-in-input-focus')
-  };
+mainInput.forEach((elem, index) => inputFocus(elem, index, signInInput, 'sign-in-input-focus'));
+mainRegistInput.forEach((elem, index) => inputFocus(elem, index, registInput, 'registration-input-focus'));
 
-  elem.onblur = () => {
-    if (elem.value == '') {
-      signInInput[index].classList.remove('sign-in-input-focus')
-    }
-  };
-})
+function inputFocus(elem, index, inputItem, classFocus) {
+  elem.onfocus = () => inputItem[index].classList.add(classFocus);
+  elem.onblur = () => elem.value == '' ? inputItem[index].classList.remove(classFocus) : elem;
+};
--- templates/regist.tpl.php
+++ templates/regist.tpl.php
@@ -4,10 +4,35 @@
   <div class="regist-header">Регистрация</div>
 
   <form class="registration-form" method="post">
-    <div class="registration-form-item"><span>Логин:</span><input name="login" type="text" required autocomplete="on" placeholder="Логин"></div>
-    <div class="registration-form-item"><span>Пароль:</span><input name="password" type="password" required autocomplete="on" placeholder="Пароль"></div>
-    <div class="registration-form-item"><span>Повторите пвроль:</span><input type="password" name="repassword" required autocomplete="on" placeholder="Подтверждение пароля"></div>
-    <div class="registration-form-item"><span>E-mail:</span><input type="email" name="email" required autocomplete="on" placeholder="E-mail"></div>
+    <div class="registration-form-item">
+      <span>Логин:</span>
+      <div class="registration-input">
+        <input class="registration-input-item" name="login" type="text" required autocomplete="on">
+        <div class="registration-placeholder">Логин</div>
+      </div>
+    </div>
+
+    <div class="registration-form-item">
+      <span>Пароль:</span>
+      <div class="registration-input">
+        <input class="registration-input-item" name="password" type="password" required autocomplete="on">
+        <div class="registration-placeholder">Пароль</div>
+      </div>
+    </div>
+    <div class="registration-form-item">
+      <span>Повторите пвроль:</span>
+      <div class="registration-input">
+        <input class="registration-input-item" type="password" name="repassword" required autocomplete="on">
+        <div class="registration-placeholder">Подтверждение пароля</div>
+      </div>
+    </div>
+    <div class="registration-form-item">
+      <span>E-mail:</span>
+      <div class="registration-input">
+        <input class="registration-input-item" type="email" name="email" required autocomplete="on">
+        <div class="registration-placeholder">E-mail</div>
+      </div>
+    </div>
     <input type="submit" name="button" value="Отправить">
   </form>
 </div>
