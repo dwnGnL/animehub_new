3c67932e723a8e5ab5a35f8934f0e58820a01b2c
DenzelHawking <denzelhawking@gmail.com>
2019-12-14 00:44:42 +0500

    questionnaire modified

--- templates/js/questionnaire.js
+++ templates/js/questionnaire.js
@@ -1,20 +1,26 @@
 let questionnairePanelItem = document.querySelectorAll('.questionnaire-panel-item');
 let questionnaireLength = document.querySelectorAll('.questionnaire-length');
 let questionnairePanelItemShadow = document.querySelectorAll('.questionnaire-panel-item-shadow');
+let questionnairePanel = document.querySelector('.questionnaire-panel');
+let questionnaireGeneralChoose = document.querySelector('.questionnaire-general-choose');
+let sumQuestionnaire = 0;
 
+for (var i = 0; i < questionnaireLength.length; i++) {
+  sumQuestionnaire += +questionnaireLength[i].dataset.length;
+};
+
+questionnaireGeneralChoose.innerHTML += sumQuestionnaire;
 
 questionnairePanelItem.forEach((elem, index) => {
   elem.onclick = () => {
-    let sumQuestionnaire = 0;
-    let onePersent = 0;
-    for (var i = 0; i < questionnaireLength.length; i++) {
-      sumQuestionnaire += +questionnaireLength[i].innerHTML;
-    };
+    if (questionnairePanel.classList.contains('questionnaire-done')) return;
 
-    onePersent = sumQuestionnaire / 100;
+    questionnairePanel.classList.add('questionnaire-done');
+    questionnairePanelItem[index].classList.add('questionnaire-choose');
 
     for (var i = 0; i < questionnairePanelItemShadow.length; i++) {
-      questionnairePanelItemShadow[i].style.width = (questionnaireLength[i].innerHTML / onePersent) + '%';
+      questionnairePanelItemShadow[i].style.width = questionnaireLength[i].dataset.length / (sumQuestionnaire / 100) + '%';
+      questionnaireLength[i].innerHTML = 'Проголосовало: ' + questionnaireLength[i].dataset.length;
     };
   };
 });
