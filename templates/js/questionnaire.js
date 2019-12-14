5fdb13474b6335d5b725bc39385d8d8d3fa5ff59
DenzelHawking <denzelhawking@gmail.com>
2019-12-14 15:19:10 +0500

    verifing choosed

--- templates/js/questionnaire.js
+++ templates/js/questionnaire.js
@@ -9,6 +9,13 @@ for (var i = 0; i < questionnaireLength.length; i++) {
   sumQuestionnaire += +questionnaireLength[i].dataset.length;
 };
 
+if (questionnairePanel.classList.contains('questionnaire-done')) {
+  for (var i = 0; i < questionnairePanelItemShadow.length; i++) {
+    questionnairePanelItemShadow[i].style.width = questionnaireLength[i].dataset.length / (sumQuestionnaire / 100) + '%';
+    questionnaireLength[i].innerHTML = questionnaireLength[i].dataset.length + ' человек';
+  };
+};
+
 questionnaireGeneralChoose.innerHTML = `Проголосовало ${sumQuestionnaire} человек`;
 
 questionnairePanelItem.forEach((elem, index) => {
