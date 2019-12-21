07c012a5ac10e5fb418fd634bc632d38c7557586
dwnGnL <992938550845@yandex.ru>
2019-12-21 15:44:56 +0500

    deleted

--- admin/js/upload.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $('#file').change(function () {
-        $('#load').append("<img src='/img/loader.gif' alert='Loading' style='margin-top: 20px;'>");
-
-        $('#sliderForm').ajaxForm({
-            target: '#load'
-        }).submit();
-    })
-});
