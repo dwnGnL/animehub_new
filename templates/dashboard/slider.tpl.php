eadb003b8ec568b62004928186191f3c16f976f8
DenzelHawking <denzelhawking@gmail.com>
2020-01-26 14:45:26 +0500

    slider's table added

--- templates/dashboard/slider.tpl.php
+++ templates/dashboard/slider.tpl.php
@@ -1,3 +1,22 @@
 <div class="container-fluid">
-<h1 class="h3 mb-4 text-gray-800">Страница слайдера</h1>
+  <table cellspacing="0">
+    <thead>
+      <tr>
+        <th class="title title-slider">Title</th>
+        <th class="img-src">Img</th>
+        <th class="button-place-header">Edit/Delete</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr>
+        <td class="title-slider">asdfasdfjdfsdfsdfsdfkadsl;kgjdfngjklsdfnbgkljsnof;asdfasdfjdfsdfsdfsdfkadsl;kgjdfngjklsdfnbgkljsnof;asdfasdfjdfsdfsdfsdfkadsl;kgjdfngjklsdfnbgkljsnof;</td>
+        <td>images/iadfadsf.jpg</td>
+        <td class="button-place">
+          <span class="edit-table-data button-table-data">Edit</span>
+          <span class="remove-table-data button-table-data">Remove</span>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </div>
