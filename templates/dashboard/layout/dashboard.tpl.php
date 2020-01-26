7bb26af866fb56b4d883fe00ec328e7c409be703
DenzelHawking <denzelhawking@gmail.com>
2020-01-26 12:25:43 +0500

    table corrected

--- templates/dashboard/layout/dashboard.tpl.php
+++ templates/dashboard/layout/dashboard.tpl.php
@@ -12,11 +12,13 @@
   <title>AnimeHub.tj - Dashboard</title>
 
   <!-- Custom fonts for this template-->
+
   <link href="<?=$uri?>/templates/dashboard/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
   <link href="<?=$uri?>/templates/dashboard/css/sb-admin-2.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="<?=$uri?>/templates/dashboard/css/table.css">
 
 </head>
 
--- templates/dashboard/posts.tpl.php
+++ templates/dashboard/posts.tpl.php
@@ -1,3 +1,51 @@
 <div class="container-fluid">
-<h1 class="h3 mb-4 text-gray-800">Страница вывода всех постов</h1>
+
+  <div class="search-table-block">
+    <div class="search-block">
+      <span>Search:</span>
+      <input type="text" name="">
+    </div>
+  </div>
+
+  <table cellspacing="0">
+    <thead>
+      <tr>
+        <th class="title">Title</th>
+        <th class="views">Views</th>
+        <th class="reating">Rating</th>
+        <th class="comments">Comments</th>
+        <th class="date">Date</th>
+        <th class="button-place-header">Edit/Delete</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr>
+        <td>asdfasdfjdfsdfsdfsdfkadsl;kgjdfngjklsdfnbgkljsnof;asdfasdfjdfsdfsdfsdfkadsl;kgjdfngjklsdfnbgkljsnof;asdfasdfjdfsdfsdfsdfkadsl;kgjdfngjklsdfnbgkljsnof;</td>
+        <td>225</td>
+        <td>10</td>
+        <td>1</td>
+        <td>25.01.2020 25:18</td>
+        <td class="button-place">
+          <span class="edit-table-data button-table-data">Edit</span>
+          <span class="remove-table-data button-table-data">Remove</span>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+
+  <div class="table-pages-place">
+    <div class="table-pages">
+      <div class="prev-table-page button-toggle-page">Previous</div>
+      <div class="page-num active">1</div>
+      <div class="page-num">2</div>
+      <div class="page-num">3</div>
+      <div class="page-num">4</div>
+      <div class="page-num">5</div>
+      <div class="page-num">6</div>
+      <div class="next-table-page button-toggle-page">Next</div>
+    </div>
+  </div>
+
+<!-- <h1 class="h3 mb-4 text-gray-800">Страница вывода всех постов</h1> -->
 </div>
