5118363898f202f19e387c3537e13e4ced185ed0
Desu-php <nonofon@vh102.timeweb.ru>
2020-03-12 20:10:52 +0300

    added images for manga

--- config.php
+++ config.php
@@ -2,15 +2,15 @@
 
 define('PREF', 'lite_');
 
-define('DB_HOST','localhost');
-define('DB_NAME','animehub');
-define('DB_USER','root');
-define('DB_PASSWORD','');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'nonofon_anime');
+define('DB_PASSWORD', 'animehub000');
+define('DB_NAME', 'nonofon_anime');
 
 define('QUANTITY', 28);
 define('QUANTITY_LINKS', 3);
 
-define('DATE', 5);
+define('DATE', 2);
 // тут пути в которых не надо проверять авторизован ли чел или нет и проверять токен
 $exception = [
     '/login',
