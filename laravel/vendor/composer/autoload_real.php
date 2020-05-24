78e3d6fe5a34f6c3e373903717659f67d094a654
Desu-php <teaddyson@gmail.com>
2020-05-24 21:21:18 +0300

    video.js change

--- laravel/vendor/composer/autoload_real.php
+++ laravel/vendor/composer/autoload_real.php
@@ -13,9 +13,6 @@ class ComposerAutoloaderInit28cb402e43508f3afe0dd399db90a3ad
         }
     }
 
-    /**
-     * @return \Composer\Autoload\ClassLoader
-     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
