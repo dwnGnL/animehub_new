382a60f4f95a39dcb5019bd31da634f2aeafc488
BAKHA <Teaddyson@gmail.com>
2020-06-20 12:37:36 +0500

    bag fixed

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+
+define('PREF', 'lite_');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'animehub');
+
+define('QUANTITY', 28);
+define('QUANTITY_LINKS', 3);
+define('DATE', 2);
+define('SITE', true);
+// тут пути в которых не надо проверять авторизован ли чел или нет и проверять токен
+$exception = [
+    '/login',
+    '/registration',
+    '/ajax/chat/getMessage',
+    '/ajax/check/auth',
+    '/ajax/chat/connect',
+    '/ajax/search/ajax',
+];
+?>
