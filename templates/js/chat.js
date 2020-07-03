f628e140df98c8a8b3c79e71333f304d4928d634
unknown <992938550845@yandex.ru>
2020-07-04 00:32:31 +0500

    add comment

--- templates/js/chat.js
+++ templates/js/chat.js
@@ -208,51 +208,51 @@ chat.ondragstart = function () {
 
 $('#sendChat').click(function () {
   var mymessage = CKEDITOR.instances['redactor'].getData();
-  var regex="/[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}"
-  var links=mymessage.match(regex)
-  console.log(mymessage)
-  console.log(links)
+  // var regex="/[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}"
+  // var links=mymessage.match(regex)
+  // console.log(mymessage)
+  // console.log(links)
 
-  if(links){
-    console.log("этап 1")
+  // if(links){
+  //   console.log("этап 1")
 
-    links.forEach(element => {
-      element=element.substr(1)
-      console.log(element)
-      if (element=="animehub.tj"||element=="/animehub.tj") return
-      let fulllink="http://"+element
-      let fulllinks="https://"+element
-      let hreflinks='href="'+element
-      let hreflinks2="href='"+element
-      let srclinks='src="'+element
-      let srclinks2="src='"+element
+  //   links.forEach(element => {
+  //     element=element.substr(1)
+  //     console.log(element)
+  //     if (element=="animehub.tj"||element=="/animehub.tj") return
+  //     let fulllink="http://"+element
+  //     let fulllinks="https://"+element
+  //     let hreflinks='href="'+element
+  //     let hreflinks2="href='"+element
+  //     let srclinks='src="'+element
+  //     let srclinks2="src='"+element
       
-      if(mymessage.indexOf(fulllink)+1>=0 
-      || mymessage.indexOf(fulllinks)+1>=0 
-      || mymessage.indexOf(hreflinks)+1>=0 
-      || mymessage.indexOf(hreflinks2)+1>=0 
-      || mymessage.indexOf(srclinks)+1>=0 
-      || mymessage.indexOf(srclinks2)+1>=0
-      || mymessage.indexOf("<object")
-      || mymessage.indexOf("<script")){
-        mymessage=""
-      }
-    });
-  }
-  mymessage=mymessage.replace("<br />","")
-  mymessage=mymessage.replace("<h1>","")
-  mymessage=mymessage.replace("</h1>","")
-  mymessage=mymessage.replace("<h2>","")
-  mymessage=mymessage.replace("</h2>","")
-  mymessage=mymessage.replace("<h3>","")
-  mymessage=mymessage.replace("</h3>","")
-  mymessage=mymessage.replace("<h4>","")
-  mymessage=mymessage.replace("</h4>","")
-  mymessage=mymessage.replace("<h5>","")
-  mymessage=mymessage.replace("</h5>","")
-  mymessage=mymessage.replace("style=","")
-  mymessage=mymessage.replace("<i>","")
-  mymessage=mymessage.replace("</i>","")
+  //     if(mymessage.indexOf(fulllink)+1>=0 
+  //     || mymessage.indexOf(fulllinks)+1>=0 
+  //     || mymessage.indexOf(hreflinks)+1>=0 
+  //     || mymessage.indexOf(hreflinks2)+1>=0 
+  //     || mymessage.indexOf(srclinks)+1>=0 
+  //     || mymessage.indexOf(srclinks2)+1>=0
+  //     || mymessage.indexOf("<object")
+  //     || mymessage.indexOf("<script")){
+  //       mymessage=""
+  //     }
+  //   });
+  // }
+  // mymessage=mymessage.replace("<br />","")
+  // mymessage=mymessage.replace("<h1>","")
+  // mymessage=mymessage.replace("</h1>","")
+  // mymessage=mymessage.replace("<h2>","")
+  // mymessage=mymessage.replace("</h2>","")
+  // mymessage=mymessage.replace("<h3>","")
+  // mymessage=mymessage.replace("</h3>","")
+  // mymessage=mymessage.replace("<h4>","")
+  // mymessage=mymessage.replace("</h4>","")
+  // mymessage=mymessage.replace("<h5>","")
+  // mymessage=mymessage.replace("</h5>","")
+  // mymessage=mymessage.replace("style=","")
+  // mymessage=mymessage.replace("<i>","")
+  // mymessage=mymessage.replace("</i>","")
 
 
   if (mymessage == "") {
