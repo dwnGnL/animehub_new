a2c7ba6b1a8a5de6d1aa6c602e8257ba46dba9d3
BAKHA <Teaddyson@gmail.com>
2020-12-11 17:09:41 +0500

    user registration disabled

--- Controller/RegistController.php
+++ Controller/RegistController.php
@@ -17,54 +17,54 @@ class RegistController extends DisplayController
         $this->display();
     }
 
-    public function registration(){
-        $userDB = new User();
-
-if (isset($_POST['button'])) {
-    $errors = [];
-    if (isset($_POST['login'])) {
-        if (trim($_POST['login']) == '') {
-            $errors[] = 'Введите логин!';
-        } elseif ((strlen(trim($_POST['login']))) <= 3) {
-            $errors[] = 'Login должен иметь более 3 символов';
-        }
-    }
-
-    if (isset($_POST['email'])) {
-        if (trim($_POST['email']) == '') {
-            $errors[] = 'Введите email!';
-        }
-    }
-
-    if (isset($_POST['password'])) {
-        if (trim($_POST['password']) == '') {
-            $errors[] = 'Введите пароль!';
-        }
-    }
-    if (isset($_POST['repassword'])) {
-        if ($_POST['password'] != $_POST['repassword']) {
-            $errors[] = 'Введённые пароли должны совпадать.!';
-        }
-        if (trim($_POST['password']) == '') {
-            $errors[] = 'Введите повторный пароль!';
-        }
-    }
-
-    }
-
-    $count = $userDB->getCountUsersLoginOrEmail($_POST['login'], $_POST['email']);
-    if ($count['COUNT(*)'] > 0) {
-        $errors [] = 'Пользователь с таким логином или почтой существует!';
-    }
-    if (empty($errors)) {
-        $userDB->addNewUser($_POST['login'], $_POST['email'], $_POST['password'],time(), $_SERVER['REMOTE_ADDR'],$this->uri);
-        $this->app->redirectTo('home');
-        echo '<p style="color:green;">Вы успешно зарегистрировались на сайте!,</p>';
-    } else {
-        echo '<p style="color: red; margin-left: 10px;">' . array_shift($errors) . '</p>';
-
-    }
-}
+//    public function registration(){
+//        $userDB = new User();
+//
+//if (isset($_POST['button'])) {
+//    $errors = [];
+//    if (isset($_POST['login'])) {
+//        if (trim($_POST['login']) == '') {
+//            $errors[] = 'Введите логин!';
+//        } elseif ((strlen(trim($_POST['login']))) <= 3) {
+//            $errors[] = 'Login должен иметь более 3 символов';
+//        }
+//    }
+//
+//    if (isset($_POST['email'])) {
+//        if (trim($_POST['email']) == '') {
+//            $errors[] = 'Введите email!';
+//        }
+//    }
+//
+//    if (isset($_POST['password'])) {
+//        if (trim($_POST['password']) == '') {
+//            $errors[] = 'Введите пароль!';
+//        }
+//    }
+//    if (isset($_POST['repassword'])) {
+//        if ($_POST['password'] != $_POST['repassword']) {
+//            $errors[] = 'Введённые пароли должны совпадать.!';
+//        }
+//        if (trim($_POST['password']) == '') {
+//            $errors[] = 'Введите повторный пароль!';
+//        }
+//    }
+//
+//    }
+//
+//    $count = $userDB->getCountUsersLoginOrEmail($_POST['login'], $_POST['email']);
+//    if ($count['COUNT(*)'] > 0) {
+//        $errors [] = 'Пользователь с таким логином или почтой существует!';
+//    }
+//    if (empty($errors)) {
+//        $userDB->addNewUser($_POST['login'], $_POST['email'], $_POST['password'],time(), $_SERVER['REMOTE_ADDR'],$this->uri);
+//        $this->app->redirectTo('home');
+//        echo '<p style="color:green;">Вы успешно зарегистрировались на сайте!,</p>';
+//    } else {
+//        echo '<p style="color: red; margin-left: 10px;">' . array_shift($errors) . '</p>';
+//
+//    }
+//}
 
 
 
