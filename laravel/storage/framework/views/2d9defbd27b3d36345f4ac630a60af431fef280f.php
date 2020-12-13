e5029c1d41968caba8d54edb5ded852ff6730943
Desu-php <teaddyson@gmail.com>
2020-12-13 09:37:35 +0300

    commit

--- laravel/storage/framework/views/2d9defbd27b3d36345f4ac630a60af431fef280f.php
+++
@@ -1,8 +0,0 @@
-<?php if(is_field_translatable($dataTypeContent, $row)): ?>
-    <input type="hidden"
-           data-i18n="true"
-           name="<?php echo e($row->field); ?>_i18n"
-           id="<?php echo e($row->field); ?>_i18n"
-           value="<?php echo e(get_field_translations($dataTypeContent, $row->field, $row->type, true)); ?>">
-<?php endif; ?>
-<?php /**PATH /home/n/nonofon/animehub/public_html/laravel/vendor/tcg/voyager/src/../resources/views/multilingual/input-hidden-bread-read.blade.php ENDPATH**/ ?>
