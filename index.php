a5d3ce0f4863e56c85ce63106f7e2b8ae1d10111
BAKHA <Teaddyson@gmail.com>
2021-03-23 14:23:15 +0500

    added eloqument laravel

--- index.php
+++ index.php
@@ -28,6 +28,33 @@ if($access != 1 && !SITE){
 	exit('Идет тех обслуживание');
 }
 
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => DB_HOST,
+    'database'  =>  DB_NAME,
+    'username'  => DB_USER,
+    'password'  => DB_PASSWORD,
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+
+// Set the event dispatcher used by Eloquent models... (optional)
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
+$capsule->setEventDispatcher(new Dispatcher(new Container));
+
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
+
+
 \Slim\Slim::registerAutoloader();
 
 $app = new \Slim\Slim([
@@ -67,6 +94,9 @@ $middle = function () {
     );
     return $obj->onBeforeDispatch();
 };
+
+include 'web/routes.php';
+
 
 $app->get('/parser', function () use ($app) {
     $o = \Controller\Controller::getInstance('parser'); //ParserController
